5c37ed6a5ef78dc049eee6e5c47f8bbec46ccda3
Youness Id bakkasse <std.youness@gmail.com>
2021-12-13 12:03:02 +0100

    Created a random dice.

--- bore/main.py
+++ bore/main.py
@@ -1,1 +1,31 @@
-print('Welcome to Bore.');
+# Target total score to win by default
+DEFAULT_TARGET_SCORE = 2000
+
+# Number of dices by default in the set
+DEFAULT_DICES_NB = 5
+# Number of side of the dices used in the game
+NB_DICE_SIDE = 6
+
+# List of dice value scoring
+LIST_SCORING_DICE_VALUE = [1, 5]
+# List of associated score for scoring dice values
+LIST_SCORING_MULTIPLIER = [100, 50]
+
+# Trigger for multiple bonus
+TRIGGER_OCCURRENCE_FOR_BONUS = 3
+# Special bonus multiplier for multiple ace bonus
+BONUS_VALUE_FOR_ACE_BONUS = 1000
+# Standard multiplier for multiple dices value bonus
+BONUS_VALUE_FOR_NORMAL_BONUS = 100
+
+class Dice:
+  def __init__(self):
+    print('Dice created..')
+    self.faces = 6
+
+  def roll (self):
+      print(f'Dice ${self.faces}..')
+
+
+dice = Dice()
+dice.roll()
