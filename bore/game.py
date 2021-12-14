e881519a4a6b21164e860d38df99c85e1ce0b540
David2he <david.dehe.pro@gmail.com>
2021-12-14 18:15:57 +0100

    Add: add player in the game and clean the code

--- bore/game.py
+++ bore/game.py
@@ -1,66 +1,116 @@
 import random
 import constants
 
+def create_player():
+    PLAYER = []
+    player_username = ""
+    how_many_player = int(input("combien de joueur êtes vous ?"))
+    print(how_many_player)
+    for index in range(how_many_player):
+        player_username = str(input("votre nom"))
+        PLAYER.append({
+            'username': player_username,
+            'score': 0,
+            'id': index,
+        })
+    return PLAYER
+
 
 def roll_dice_set(how_many_dice):
-    constants.THROW_DICE_COUNTER = 0
+
     dice_value_list = [0] * constants.NB_DICE_SIDE
     for n in range(how_many_dice):
         dice_value = random.randint(1, constants.NB_DICE_SIDE)
         dice_value_list[dice_value - 1] += 1
         print(dice_value)
-    print("----")
-    print(dice_value_list)
-    print("----")
-    return analyse_bonus_score(dice_value_list)
 
-def analyse_bonus_score(dice_value_list):
-    bonus_score = 0
-    for index, dice_value in enumerate(dice_value_list):
-        nb_of_bonus = dice_value // constants.TRIGGER_OCCURRENCE_FOR_BONUS
-        if nb_of_bonus > 0:
-            if index == 0:
-                bonus_mulitplier = constants.BONUS_VALUE_FOR_ACE_BONUS
-            else:
-                bonus_mulitplier = constants.BONUS_VALUE_FOR_NORMAL_BONUS
+    return dice_value_list
 
-            bonus_score += nb_of_bonus * bonus_mulitplier * (index + 1)
-            dice_value_list[index] %= constants.TRIGGER_OCCURRENCE_FOR_BONUS
-    return analyse_standard_score(dice_value_list, bonus_score)
+def analyse_bonus_score(dice_value_list, player):
+   print(dice_value_list)
 
-def analyse_standard_score(dice_value_list, bonus_score):
+   bonus_score = 0
+   for index, dice_value in enumerate(dice_value_list):
+       nb_of_bonus = dice_value // constants.TRIGGER_OCCURRENCE_FOR_BONUS
+       if nb_of_bonus > 0:
+           if index == 0:
+               bonus_mulitplier = constants.BONUS_VALUE_FOR_ACE_BONUS
+           else:
+               bonus_mulitplier = constants.BONUS_VALUE_FOR_NORMAL_BONUS
+
+           bonus_score += nb_of_bonus * bonus_mulitplier * (index + 1)
+           dice_value_list[index] %= constants.TRIGGER_OCCURRENCE_FOR_BONUS
+
+   print(bonus_score)
+   player["score"] += bonus_score
+
+   return dice_value_list
+
+
+def analyse_standard_score(dice_value_list, player):
     standard_score = 0
     for scoring_value, scoring_multiplier in zip(constants.LIST_SCORING_DICE_VALUE, constants.LIST_SCORING_MULTIPLIER):
         standard_score += dice_value_list[scoring_value - 1] * scoring_multiplier
         dice_value_list[scoring_value - 1] = 0
-    return analyse_score(dice_value_list, standard_score, bonus_score)
 
-def analyse_score(dice_value_list, standard_score, bonus_score):
-    constants.STACK_BONUS += (bonus_score + standard_score)
-    calculus_dickus = 0
-    print(constants.STACK_BONUS)
-    print("----")
-    print(dice_value_list)
-    print("----")
-    print("----------------------------------")
+    print(standard_score)
+    player["score"] += standard_score
+    return dice_value_list
+
+
+
+
+
+def analyse_score(dice_value_list, old_score, player):
+    old_score
+    turn_is_scored = True
+    total_dice_remain_calcul = 0
+    constants.THROW_DICE_COUNTER = 0
+
+    if old_score == player["score"]:
+        turn_is_scored = False
 
     for n in dice_value_list:
-        if n != 0 and (bonus_score + standard_score) > 0:
+        if n != 0 and turn_is_scored == True:
             constants.THROW_DICE_COUNTER += n
-        calculus_dickus += n
+        total_dice_remain_calcul += n
 
-    if calculus_dickus == 0 and (bonus_score + standard_score) > 0:
+    if total_dice_remain_calcul == 0 and turn_is_scored == True:
         constants.THROW_DICE_COUNTER += 6
-        print("test")
-    elif bonus_score + standard_score == 0:
+    elif turn_is_scored == False:
+        constants.STACK_BONUS = 0
         print("perdu sale chien")
 
+
+    print("il te reste " + str(constants.THROW_DICE_COUNTER) + " dés a lancer")
     if constants.THROW_DICE_COUNTER > 0:
-        test = input("voulez vous rellancez ?")
-        if test == "yes":
-            roll_dice_set(constants.THROW_DICE_COUNTER)
+        test = input("voulez vous relancez ?")
+        if test == "y":
+            # re start game
         else:
             print("votre score pour ce tour est de " + str(constants.STACK_BONUS))
 
 
-roll_dice_set(constants.THROW_DICE_COUNTER)
+
+def game():
+    players = create_player()
+    for player in players:
+        old_score = player["score"]
+        dice_value_list = roll_dice_set(constants.THROW_DICE_COUNTER)
+        dice_value_list = analyse_bonus_score(dice_value_list, player)
+        dice_value_list = analyse_standard_score(dice_value_list, player)
+        analyse_score(dice_value_list, old_score, player)
+
+        # analyse_bonus_score(roll_dice_set(constants.THROW_DICE_COUNTER, player), player)
+        #analyse_standard_score()
+
+    print("-----------------")
+    print(players)
+
+
+
+
+
+game()
+
+
