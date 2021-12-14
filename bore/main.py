4d25d5513e22d334caa5a8369668d39b6df37f10
Youness Id bakkasse <std.youness@gmail.com>
2021-12-15 00:39:40 +0100

    Separated classes into modules.

--- bore/main.py
+++ bore/main.py
@@ -2,7 +2,12 @@ from game import Game
 
 
 if __name__ == "__main__":
+    # Initiate the game
     game = Game()
+
+    # Run the game
     game.lunch()
+
+    # Output the final results
     game.score()
 
