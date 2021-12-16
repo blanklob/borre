569dcfd09bf0880e2550d6e2c0ef7d7949b2eae2
Youness Id bakkasse <std.youness@gmail.com>
2021-12-16 22:49:54 +0100

    Added more checks.

--- bore_game/__init__.py
+++ bore_game/__init__.py
@@ -1,6 +1,6 @@
 """Bore is a Dice game"""
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 
 from .main import Bore as Bore
--- tests/test_bore_version.py
+++ tests/test_bore_version.py
@@ -5,5 +5,5 @@ def test_bore_version():
     """
     Tests the version number
     """
-    assert bore_game.__version__ == "0.0.1"
+    assert bore_game.__version__ == "0.1.0"
 
