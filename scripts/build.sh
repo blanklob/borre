c201550d10f412b46599f118422406029ecb8f5a
Youness Id bakkasse <std.youness@gmail.com>
2021-12-17 03:50:16 +0100

    Added setup.py for easy local installation.

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,6 @@
 
 set -x
 
-if [ -d 'bore-game-*' ] ; then
-  rm -r bore-game-*
-fi
-
 poetry build -n
 tar -xvf dist/*.tar.gz '*/setup.py'
 cp $(find . -name setup.py) setup.py
-mv bore-game-*/setup.py ..
